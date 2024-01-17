7e075e5977bf2bb7647d297e8ed8c48be413c79a
TimK <156866464+Timka112@users.noreply.github.com>
2024-01-16 18:00:38 -0800

    Create How is your day?.py

---
+++ How is your day?.py
@@ -0,0 +1,69 @@
+num = ("how was your day doing 1-10")
+answer = "hi"
+name = input(" What is your name")
+if name == " Johny":
+  print("get lost")
+exit()
+if name == " Billy":
+  print("oh hi")
+  if answer == "hi":
+    num = input("how was your day doing 1-10")
+    if num == " 1":
+      print("bro u good?")
+    if num == " 2":
+      print("oof must've been bad")
+    if num == " 3":
+      print("darn must've been a horrible day for you")
+    if num == " 4":
+      print("welp atleast it isnt a 2")
+    if num == " 5":
+      print(
+        "I mean sure it could be better but atleast you didnt have a day any worse it can only go up right...?"
+      )
+    if num == " 6":
+      print("well i guess the day wasent that bad")
+    if num == " 7":
+      print("seems like you had a relativly good day")
+    if num == " 8":
+      print("wow seems like you had a pretty awesome day")
+    if num == " 9":
+      print("wow you had a great day")
+    if num == " 10":
+      print("oh my you had an AMAZING day")
+  else:
+    print("thats not an answer choice dummy")
+answer2 = "hello"
+if name == " Mom":
+  print("oh hi mom")
+  if answer2 == "hello":
+    num = input("how was your day doing 1-10")
+    if num == " 1":
+      print("jeez mom are you okay?")
+    if num == " 2":
+      print("oof must've been bad")
+    if num == " 3":
+      print("darn must've been a horrible day for you")
+    if num == " 4":
+      print("welp atleast it isnt a 2")
+    if num == " 5":
+      print(
+        "I mean sure it could be better but atleast you didnt have a day any worse it can only go up right...?"
+      )
+    if num == " 6":
+      print("well i guess the day wasent that bad")
+    if num == " 7":
+      print("seems like you had a relativly good day")
+    if num == " 8":
+      print("wow seems like you had a pretty awesome day")
+    if num == " 9":
+      print("wow you had a great day")
+    if num == " 10":
+      print("oh my you had an AMAZING day")
+num3 = input("was your day going from 1-5?")
+if num3 == "yes":
+  num4 = input("Do you think you can make it better?")
+  if num4 == "yes":
+    print("Awesome!")
+if num3 == "no":
+  print("Then that is Awesome! Keep it up")
+print("Well good day or not it was still a day right?")
